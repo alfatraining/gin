5d6138a02bb90f096f66327ba64924fa6dd7adaa
Andreas Jaekle <andreas@jaekle.net>
2019-08-23 14:09:14 +0200

    fix broken gin.Context go.Context implementation

--- context.go
+++ context.go
@@ -545,15 +545,15 @@ func (c *Context) SetAccepted(formats ...string) {
 /************************************/
 
 func (c *Context) Deadline() (deadline time.Time, ok bool) {
-	return
+	return c.Request.Context().Deadline()
 }
 
 func (c *Context) Done() <-chan struct{} {
-	return nil
+	return c.Request.Context().Done()
 }
 
 func (c *Context) Err() error {
-	return nil
+	return c.Request.Context().Err()
 }
 
 func (c *Context) Value(key interface{}) interface{} {
@@ -561,8 +561,9 @@ func (c *Context) Value(key interface{}) interface{} {
 		return c.Request
 	}
 	if keyAsString, ok := key.(string); ok {
-		val, _ := c.Get(keyAsString)
-		return val
+		if val, ok := c.Get(keyAsString); ok {
+			return val
+		}
 	}
-	return nil
+	return c.Request.Context().Value(key)
 }
